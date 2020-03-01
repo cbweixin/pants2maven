a4f1d65a20e92c0be96ff153364e647736633142
cbweixin <cbweixin@qq.com>
2020-02-29 16:03:41 -0800

    refactor code

--- build2pom/src/main/java/com/weixin/utils/Utils.java
+++ build2pom/src/main/java/com/weixin/utils/Utils.java
@@ -5,9 +5,6 @@ import org.stringtemplate.v4.STGroupFile;
 
 public class Utils {
   public static String basePath = "/Users/xinwei/Documents/tinder/github/palo";
-  public static STGroup stg = new STGroupFile(
-      "/Users/xinwei/Documents/weixin/study-antlr/antlr-ex/src/main/java/com/weixin/pants/pom.stg");
-
 
   public static String stripSingleQuotes(String s) {
     if (s == null || s.charAt(0) != '\'') {
